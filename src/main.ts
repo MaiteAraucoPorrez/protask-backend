9a4552cb3c4349cf7ea83d50e49e32b6eccb5a95
Maite Arauco Porrez <gabriela.arauco.p@ucb.edu.bo>
2026-05-12 18:11:08 -0400

    feat: base del proyecto - auth y users completo

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-  console.log(`🚀 ProTask API corriendo en http://localhost:${port}/api`);
+  console.log(`ProTask API corriendo en http://localhost:${port}/api`);
 }
 bootstrap();
